9af941291be2d3107afe278c012521e12e7cd878
Juan Camilo Herrera Ardila <Juan.Herrera@bitsamericas.com>
2023-11-22 22:27:08 -0500

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController
+  ) {}
+
+  async openPage() {
+    const alert = await this.alertCtrl.create({
+      header: 'Products',
+      message: 'You have 300 products',
+      buttons: ['Ok']
+    });
+
+    await alert.present();
+  }
+
+  async openLoading() {
+    const  loading = await this.loadingCtrl.create({
+      message: 'Cargando carrito...',
+      spinner: 'crescent',
+      duration: 2000
+    })
+
+    await loading.present();
+  }
 
 }
